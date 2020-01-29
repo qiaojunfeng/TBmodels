8f34bea9f3ddda43a399766d86b7e2bd8ac93b6e
Junfeng Qiao <qiaojunfeng@outlook.com>
2020-01-29 12:29:13 +0100

    Add CLI for calculating DOS

---
+++ examples/cmdline/dos/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# (c) 2015-2018, ETH Zurich, Institut fuer Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+
+tbmodels dos -i input/silicon_model.hdf5 -k 10 10 10 -e -10 20 -n 1000 -s 0 -w 0.1 -o silicon_dos.hdf5
